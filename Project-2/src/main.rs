4b2ae1505892ef07c77d7bdba0bdad0e85c3cb95
Brage Kvamme <brage.kvamme@gmail.com>
2025-02-14 13:55:07 +0100

    folder

--- Project-2/src/main.rs
+++ Project-2/src/main.rs
@@ -1,4 +1,5 @@
 mod io;
+mod structs;
 
 fn main() {
     let inf = io::read_from_json("train/train_0.json").unwrap();
---
+++ Project-2/target/debug/build/serde_json-cdcc2fd20450c231/build_script_build-cdcc2fd20450c231.d
@@ -0,0 +1,5 @@
+/home/brage/Documents/ntnufag/Bio-AI/Project-2/target/debug/build/serde_json-cdcc2fd20450c231/build_script_build-cdcc2fd20450c231: /home/brage/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_json-1.0.138/build.rs
+
+/home/brage/Documents/ntnufag/Bio-AI/Project-2/target/debug/build/serde_json-cdcc2fd20450c231/build_script_build-cdcc2fd20450c231.d: /home/brage/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_json-1.0.138/build.rs
+
+/home/brage/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_json-1.0.138/build.rs:
